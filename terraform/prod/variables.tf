aae29f5cfbe10ccfd797e2ad1dedf6ba3db36b79
JoeChapman <joseph.philip.chapman@gmail.com>
2019-04-24 17:47:27 +0100

    Clean up fodlers and files after poor commit management

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -1,10 +1,2 @@
-variable "cluster-name" {
-  default = "savvy"
-  type    = "string"
-}
-
 variable "app_version" {}
 variable "aws_region" {}
-variable "environment" {}
-variable "project" {}
-variable "team" {}
